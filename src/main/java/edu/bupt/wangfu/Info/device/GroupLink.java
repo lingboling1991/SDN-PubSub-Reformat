ddc10cbcfafbec613b74843a40cacc7ca5708783
lingboling1991 <lingboling1991@126.com>
2016-11-14 22:50:18 +0800

    集群间订阅管理

--- src/main/java/edu/bupt/wangfu/Info/device/GroupLink.java
+++ src/main/java/edu/bupt/wangfu/Info/device/GroupLink.java
@@ -4,8 +4,8 @@ package edu.bupt.wangfu.info.device;
  * Created by lenovo on 2016-10-31.
  */
 public class GroupLink {
-	public String srcGroupName;//自己这边的groupName
-	public String dstGroupName;//对面的集群名称
+	public String srcGroupName;//发信人自己这边的groupName
+	public String dstGroupName;//发信人对面的集群名称
 
 	public String srcBorderSwtId;//自己这边，边界swtId
 	public String srcOutPort;
